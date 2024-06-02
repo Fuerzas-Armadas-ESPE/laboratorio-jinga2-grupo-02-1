8306963015242e97188368dc039f7a5df97bbbfd
Leonardo <jlobando2@espe.edu.ec>
2024-06-01 22:52:32 -0500

    Vistas listar, editar, eliminar y crear

--- productos/urls.py
+++ productos/urls.py
@@ -4,5 +4,11 @@ from . import views
 urlpatterns = [
     # URLs de vistas normales
     # Crear la URL de la vista index
-    path('', views.listar_productos, name='listar_productos'),
+    path('',views.base, name='base'),       
+    path('listar/', views.listar_productos, name='listar_productos'),
+    path('crear/', views.crear, name='crear'),
+    path('listar_editar/', views.listar_editar, name='listar_editar'),
+    path('editar/<int:id>/', views.editar, name='editar'),
+     path('eliminar/', views.eliminar, name='eliminar_productos'),  
+    path('eliminar/<int:id>/', views.eliminar_elemento, name='eliminar_producto'),  
 ]
--- productos/views.py
+++ productos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect, get_object_or_404
 from .models import Producto
 
 productos = []
@@ -8,3 +8,41 @@ def listar_productos(request):
     # Renderiza la plantilla listar.html
     productos = Producto.objects.all()
     return render(request, 'listar.html', {'productos': productos})
+
+def base(request):
+    return render(request, 'base.html')
+
+def crear(request):
+    if request.method == 'POST':
+        nombre = request.POST['nombre']
+        precio = request.POST['precio']
+        cantidad = request.POST['cantidad']
+        Producto.objects.create(nombre=nombre, precio=precio, cantidad=cantidad)
+        return render(request, 'base.html', {'mensaje': 'Producto creado correctamente'})
+    return render(request, 'crear.html')
+
+def editar(request, id):
+    producto = Producto.objects.get(id=id)
+    if request.method == 'POST':
+        nombre = request.POST['nombre']
+        precio = request.POST['precio']
+        cantidad = request.POST['cantidad']
+        producto.nombre = nombre
+        producto.precio = precio
+        producto.cantidad = cantidad
+        producto.save()
+        return render(request, 'base.html', {'mensaje': 'Producto editado correctamente'})
+    return render(request, 'editar.html', {'producto': producto})
+
+def listar_editar(request):
+    productos = Producto.objects.all()
+    return render(request, 'listar_editar.html', {'productos': productos})
+
+def eliminar(request):
+    productos = Producto.objects.all()
+    return render(request, 'eliminar.html', {'productos': productos})
+
+def eliminar_elemento(request, id):
+    producto = get_object_or_404(Producto, id=id)
+    producto.delete()
+    return redirect('eliminar_productos')
